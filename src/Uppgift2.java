d0e2cc5170526dc2901c3764336d4f42fcfdd975
Micho421 <michelelias333@hotmail.com>
2018-09-14 15:00:16 +0200

    Hej

---
+++ src/Uppgift2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Uppgift2 {
+
+		public static void main(String[] args) {
+		
+	int tal1 = 1;
+	int tal2 = 5;
+	int tal3 = 8;
+	int tal4 = 9;
+	int tal5 = 2;
+	int tal6 = 22;
+	int tal7 = 21;
+	int tal8 = 0;
+	int tal9 = -5;
+	int tal10 = 11;
+
+	
+	}
+	
+	
+
+}
