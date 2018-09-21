e8f49d075848241ac148ff1e9df373b97ac6b179
Micho421 <michelelias333@hotmail.com>
2018-09-21 14:38:32 +0200

    JAAAAAAAAAA

---
+++ src/kattis/r2.java
@@ -0,0 +1,18 @@
+package kattis;
+
+import java.util.Scanner;
+
+public class r2 {
+
+	public static void main (String[]args) {
+	Scanner input = new Scanner(System.in);
+	
+				int r2;
+				int s = input.nextInt();
+				int r1 = input.nextInt();
+
+
+		System.out.println(r1 * 2 - s);
+}
+
+}
